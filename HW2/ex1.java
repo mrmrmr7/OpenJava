cba1e9ebc4a27a8cb1b26f8a77e0e9fb908452d6
mrmrmr7 <aliex.s@yandex.by>
2018-11-08 02:11:36 +0300

    08/11/2018/02:10/Fix 2_1

--- HW2/ex1.java
+++ HW2/ex1.java
@@ -2,38 +2,47 @@ package CLASSES.HW2;
 import java.util.Scanner;
 
 public class ex1 {
-    public static void main(String[] args){
-        Point p1 = new Point();
-        Point p2 = new Point();
-        Point p3 = new Point();
-        double dist12, dist23, dist31;
-        dist31 = p3.distanceTo(p1);
-        dist12 = p1.distanceTo(p2);
-        dist23 = p2.distanceTo(p3);
-        if((dist12 + dist31 > dist23) || (dist12 + dist23 > dist31) || (dist31 + dist23 > dist12)){
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        Point p1 = new Point(in.nextInt(), in.nextInt());
+        Point p2 = new Point(in.nextInt(), in.nextInt());
+        Point p3 = new Point(in.nextInt(), in.nextInt());
+        in.close();
+        
+        if (((p1.disTo(p2) + p3.disTo(p1) - p2.disTo(p3)) > 1e-4) || 
+            ((p1.disTo(p2) + p2.disTo(p3) - p3.disTo(p1)) > 1e-4) ||
+            ((p3.disTo(p1) + p2.disTo(p3) - p1.disTo(p2)) > 1e-4)) {
             System.out.println("Треугольник невозможен");
-        }
-        if(Point.pifagor(dist12, dist23) == dist31 || Point.pifagor(dist12, dist31) == dist23 || Point.pifagor(dist31, dist23) == dist12){
+        } else if (Point.pifagor(p1.disTo(p2), p2.disTo(p3)) == p3.disTo(p1) || 
+                   Point.pifagor(p1.disTo(p2), p3.disTo(p1)) == p2.disTo(p3) || 
+                   Point.pifagor(p3.disTo(p1), p2.disTo(p3)) == p1.disTo(p2)) {
             System.out.println("Прямоугольный");
-        }
-        else{
+        } else {
             System.out.println("Не прямоугольный");
         }
     }
 }
 
-class Point{
+class Point {
     public int x;
     public int y;
-    Point(){
-        x=0;
-        y=0;
+
+    Point() {
+        this(0,0);
     }
-    public double distanceTo(Point _a){
+
+    Point(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+
+    public double disTo(Point _a) {
         double res = Math.pow(Math.pow(_a.x - this.x,2) + Math.pow(_a.y - this.y,2), 1./2.);
         return res;
     }
-    public static double pifagor(double d1, double d2){
+    
+    public static double pifagor(double d1, double d2) {
         return Math.sqrt(Math.pow(d1,2) + Math.pow(d2,2));
     }
 }
