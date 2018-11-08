6d778536ded3cd0074ae55addbc586acd558ce35
Alexandr Zaporozhtsev <aliex.s@yandex.by>
2018-11-08 19:21:10 +0300

    Update ex1.java

--- HW1/ex1.java
+++ HW1/ex1.java
@@ -1,16 +1,23 @@
 package CLASSES.HW1;
+import java.util.Scanner;
 
 public class ex1{
     public static void main(String[] arg) {
-        int a;
-        int b;
-        int c;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        
+        Scanner in = new Scanner(System.in);
+        try {
+            a = in.nextInt();
+            b = in.nextInt();
+            c = in.nextInt();
+        } catch (inputmismatchexception e) {
+            System.out.println("Wrong type")
+        }
+        in.close();
+        
+        System.out.println((a == b && b == c) ? "они одинаковые" : "они разные");
 
-        if (a == b && b == c){
-            System.out.println("они одинаковые");
-        }
-        else{
-            System.out.println("они разные");
-        }
     }
 }
